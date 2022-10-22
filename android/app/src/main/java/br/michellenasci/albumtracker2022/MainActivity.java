de1938c3435682583291925a7e286e934492819c
MichelleNascimento2000 <michelle.nascimento.silva@outlook.com>
2022-10-21 21:07:32 -0300

    Adicionando Android

---
+++ android/app/src/main/java/br/michellenasci/albumtracker2022/MainActivity.java
@@ -0,0 +1,5 @@
+package br.michellenasci.albumtracker2022;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Qatar2022AlbumTracker',
+  appId: 'br.michellenasci.albumtracker2022',
+  appName: 'Qatar 2022 Album Tracker',
   webDir: 'www',
   bundledWebRuntime: false
 };
